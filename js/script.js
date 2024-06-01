d425de9d4b84192aa88c2577b53069e0fd3a5222
LuccaMilanoDev <luccamilano2018@gmail.com>
2024-06-01 12:19:06 -0300

    finish js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,22 @@
 const img = document.getElementById("product-image");
+const buttons = document.querySelectorAll("#image-picker li");
+
+buttons.forEach((btn) => {
+  btn.addEventListener("click", (e) => {
+    buttons.forEach((btn) => {
+      btn.querySelector(".color").classList.remove("selected");
+    });
+
+    const buttonClick = e.target;
+    buttonClick.querySelector(".color").classList.add("selected");
+
+    const id = buttonClick.getAttribute("id");
+
+    img.classList.add("changing");
+    img.setAttribute("src", `img/iphone_${id}.png`);
+
+    setTimeout(() => {
+      img.classList.toggle("changing");
+    }, 200);
+  });
+});
